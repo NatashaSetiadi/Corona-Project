4a5aa1c29e7373d928769b50607442d79181d10c
EBruda <elizabethb10124@gmail.com>
2020-05-28 01:42:23 -0400

    Added the VueJS component for the symptom checker

---
+++ Symptom_Script.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		symptoms: [
+			{
+				text: "Dry cough",
+				point: 0
+			},
+			{
+				text: "Fever greater than 100",
+				point: 0
+            },
+            {
+                text: "New loss of taste or smell",
+                point: 0
+            },
+            {
+                text: "Chills",
+                point: 0
+            },
+
+            {
+                text: "Muscle pain",
+                point: 0
+            }
+
+            {
+                text: "Shortness of breath",
+                point: 0
+            }
+		]
+	}
+})
+
